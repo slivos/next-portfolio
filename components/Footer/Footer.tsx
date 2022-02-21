0739cf6750c06e8a53ea24fd788bc76ec4ef95df
slivos.frontend <77125423+blue-water12@users.noreply.github.com>
2022-02-21 16:09:49 +0100

    Change year

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,51 +3,49 @@ import { FooterContainer, FooterWrapper, FooterUl, FooterLink, SocialLinkGit } f
 
 import { FiGithub } from 'react-icons/fi';
 
-
-
-
-export const Footer: React.FC = ({
-
-}) => {
+export const Footer: React.FC = ({}) => {
     return (
         <>
             <FooterContainer>
                 <FooterWrapper>
                     <FooterUl>
-                            <li>
+                        <li>
                             <FooterLink to="domov" spy={true} smooth={true} duration={1000}>
                                 DOMOV
                             </FooterLink>
-                            </li>
-                            <li>
+                        </li>
+                        <li>
                             <FooterLink to="o-mne" spy={true} smooth={true} duration={1000}>
                                 O MNE
                             </FooterLink>
-                            </li>
-                            <li>
+                        </li>
+                        <li>
                             <FooterLink to="kontakt" spy={true} smooth={true} duration={1000}>
                                 KONTAKT
                             </FooterLink>
-                            </li>
+                        </li>
                     </FooterUl>
                 </FooterWrapper>
-                <small>&copy; Designed by slivos, 2021</small>
+                <small>&copy; Designed by slivos, 2022</small>
                 <FooterWrapper>
                     <FooterUl>
                         <li>
                             <div>+421 905 495 677</div>
                         </li>
                         <li>
-                           <div>slivos.frontend@gmail.com</div>
+                            <div>slivos.frontend@gmail.com</div>
                         </li>
                         <li>
-                        <SocialLinkGit href="http://github.com/slivos/next-portfolio" target="_blank" rel="noopener noreferrer">
-                            <FiGithub />
-                        </SocialLinkGit>
+                            <SocialLinkGit
+                                href="http://github.com/slivos/next-portfolio"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <FiGithub />
+                            </SocialLinkGit>
                         </li>
                     </FooterUl>
                 </FooterWrapper>
-                
             </FooterContainer>
         </>
     );
