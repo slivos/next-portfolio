745eac16661f8697c80d17de2bd39aa6b7846a14
slivos.frontend <77125423+blue-water12@users.noreply.github.com>
2022-02-22 22:57:00 +0100

    Update styles for ProjectItem

--- components/Projects/ProjectItem.styled.tsx
+++ components/Projects/ProjectItem.styled.tsx
@@ -6,6 +6,12 @@ export const TextWrapper = styled.div`
     align-items: center;
 `;
 
+export const Text = styled.span`
+    font-size: 1.2rem;
+    font-weight: 700;
+    color: white;
+`;
+
 export const Item = styled.div`
     width: 20em;
     height: 10em;
@@ -19,19 +25,14 @@ export const Item = styled.div`
 
     &:hover {
         box-shadow: 5px 5px 9px 5px rgba(0, 0, 0, 0.45);
-        background: url('images/falcon-eshop.PNG');
-        opacity: 0.4;
+        background: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.7)),
+            url('images/falcon-eshop.PNG') center center/cover no-repeat;
     }
 
-    &:hover > ${TextWrapper} {
-        display: none;
+    &:hover ${Text} {
+        color: #66fcf1;
+        font-weight: 800;
     }
-`;
-
-export const Text = styled.span`
-    font-size: 1.2rem;
-    font-weight: 700;
-    color: white;
 `;
 
 export const SendTo = styled.a`
