a73889e700c408ee0a656fbec40051dda7e86cb5
slivos.frontend <77125423+blue-water12@users.noreply.github.com>
2022-02-22 09:19:07 +0100

    Commit unchanged files

--- components/Arrow/Arrow.tsx
+++ components/Arrow/Arrow.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import { StyledArrow } from './Arrow.styled';
 
-
-
-
-
 export const Arrow: React.FC = () => {
-
-    return(
+    return (
         <StyledArrow>
             <span />
             <span />
             <span />
         </StyledArrow>
-    )
+    );
 };
--- components/Header/Header.styled.tsx
+++ components/Header/Header.styled.tsx
@@ -5,39 +5,30 @@ export const StyledHeader = styled.header`
     z-index: 100;
     width: 100%;
     height: 60px;
-    background-color: #66FCF1;
-    color: #131D4D;
+    background-color: #66fcf1;
+    color: #131d4d;
     display: flex;
     justify-content: space-between;
     align-items: center;
     box-shadow: 0 0 5px #5e5b5b;
-   
-@media (max-width: ${({ theme }) => theme.mobileL}) {
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-    
-}   
-`  
+`;
 
 export const Ul = styled.ul`
-   list-style: none;
-   padding: 0;
-   margin-left: 20px;
-   display: flex;
-   justify-content: center;
-   align-items: center;
-   
-@media (max-width: ${({ theme }) => theme.mobileL}) {
-       display: none;
-}
-`
+    list-style: none;
+    padding: 0;
+    margin-left: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    @media (max-width: ${({ theme }) => theme.mobileL}) {
+        display: none;
+    }
+`;
 
 export const Li = styled.li`
     margin: 0 30px;
-    
-`
-
+`;
 
 export const StyledAnchor = styled.a`
     text-decoration: none;
@@ -46,14 +37,11 @@ export const StyledAnchor = styled.a`
     font-weight: 650;
     transition: color 0.3s linear;
     letter-spacing: 0.5px;
-    
 
     &:hover {
         color: #6c757d;
-        
     }
-    
-`
+`;
 
 export const Logo = styled.span`
     margin-right: 20px;
@@ -61,7 +49,7 @@ export const Logo = styled.span`
     font-weight: 700;
     cursor: pointer;
 
-@media (max-width: ${({ theme }) => theme.mobileL}) {
-    font-size: 1.5rem;
-}    
-`
+    @media (max-width: ${({ theme }) => theme.mobileL}) {
+        font-size: 1.5rem;
+    }
+`;
