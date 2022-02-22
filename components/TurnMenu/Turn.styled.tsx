4a0a47cf3f20f3140ba40460918613e92b1a4138
slivos.frontend <77125423+blue-water12@users.noreply.github.com>
2022-02-22 08:57:02 +0100

    Add styles for Turn.tsx

---
+++ components/TurnMenu/Turn.styled.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const TurnWrapper = styled.div`
+    z-index: 500;
+    margin-left: 20px;
+
+    @media (min-width: 961px) {
+        display: none;
+    }
+`;
