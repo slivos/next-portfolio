7fa9ffd5cd77d0af717ae616913bd23f547b4a49
slivos <79216586+slivos@users.noreply.github.com>
2022-02-22 08:37:15 +0000

    Deploying to gh-pages from @ slivos/next-portfolio@db63149d9ccc656da5bbf7ff4a160e65de295355 🚀

--- _next/static/aAlfzBkJn5Tv04DbDTp04/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/260-b7659954bd989e231803.js","static/chunks/pages/index-9ecc72a0fe1d2126bf5d.js"],"/_error":["static/chunks/pages/_error-bdd8085708439f10214b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
