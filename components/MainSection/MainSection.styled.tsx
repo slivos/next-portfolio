b9d12c882a90e168c4f8b98df3bd20f58e044624
slivos.frontend <77125423+blue-water12@users.noreply.github.com>
2022-08-27 15:37:55 +0200

    add styles for social icons

--- components/MainSection/MainSection.styled.tsx
+++ components/MainSection/MainSection.styled.tsx
@@ -3,126 +3,112 @@ import styled from 'styled-components';
 export const MainSecContainer = styled.div`
     height: 100vh;
     width: 100%;
-    background: linear-gradient( 
-    rgba(19,29,77,0.6),
-    rgba(19,29,77,0.6)
-    ),
-    url("/images/img1.jpg") center center/cover
-    no-repeat;
+    background: linear-gradient(rgba(19, 29, 77, 0.6), rgba(19, 29, 77, 0.6)),
+        url('/images/img1.jpg') center center/cover no-repeat;
     background-attachment: fixed;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
 
-@media (max-width: ${({ theme }) => theme.mobileM}) {
-    background-attachment: unset; 
-}
-`
+    @media (max-width: ${({ theme }) => theme.mobileM}) {
+        background-attachment: unset;
+    }
+`;
 
 export const SectionWelcome = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-
-`    
+`;
 
 export const StyledH1 = styled.h1`
     font-size: 10rem;
     font-weight: bold;
     margin: 0;
-    color: #66FCF1;
+    color: #66fcf1;
     margin-top: -100px;
 
-@media (max-width: ${({ theme }) => theme.mobileL}) and (orientation: landscape) {
-    font-size: 6rem;
-    margin-top: 0px;
-}
+    @media (max-width: ${({ theme }) => theme.mobileL}) and (orientation: landscape) {
+        font-size: 6rem;
+        margin-top: 0px;
+    }
 
-@media (max-width: ${({ theme }) => theme.mobileM}) {
-    font-size: 8rem;
-}
+    @media (max-width: ${({ theme }) => theme.mobileM}) {
+        font-size: 8rem;
+    }
 
-@media (max-width: ${({ theme }) => theme.mobileM}) and (orientation: landscape) {
-    font-size: 6rem;
-    margin-top: 0px;
-}
+    @media (max-width: ${({ theme }) => theme.mobileM}) and (orientation: landscape) {
+        font-size: 6rem;
+        margin-top: 0px;
+    }
 
-@media (max-width: ${({ theme }) => theme.mobileXS}) {
-    font-size: 6rem;
-}
+    @media (max-width: ${({ theme }) => theme.mobileXS}) {
+        font-size: 6rem;
+    }
 
-@media (max-width: ${({ theme }) => theme.mobileXS}) and (orientation: landscape) {
-    font-size: 5rem;
-}
+    @media (max-width: ${({ theme }) => theme.mobileXS}) and (orientation: landscape) {
+        font-size: 5rem;
+    }
 
-@media (max-width: ${({ theme }) => theme.mobileXXS}) {
-    font-size: 5rem;
-}
-`
+    @media (max-width: ${({ theme }) => theme.mobileXXS}) {
+        font-size: 5rem;
+    }
+`;
 
 export const StyledP = styled.p`
     font-size: 1.5rem;
     padding: 0px 5px;
     font-weight: 700;
     text-align: center;
-    margin: 0;
-    color: #C5C6C7;
+    margin: 0 0 30px;
+    color: #c5c6c7;
 
-@media (max-width: ${({ theme }) => theme.mobileL}) and (orientation: landscape) {
-    font-size: 1.2rem;
-    margin-top: -20px;
-}
+    @media (max-width: ${({ theme }) => theme.mobileL}) and (orientation: landscape) {
+        font-size: 1.2rem;
+        margin-top: -20px;
+    }
 
-@media (max-width: ${({ theme }) => theme.mobileM}) {
-    font-size: 1.3rem;
-}
+    @media (max-width: ${({ theme }) => theme.mobileM}) {
+        font-size: 1.3rem;
+    }
 
-@media (max-width: ${({ theme }) => theme.mobileM}) and (orientation: landscape) {
-    font-size: 1.2rem;
-    margin-top: -20px;
-}
+    @media (max-width: ${({ theme }) => theme.mobileM}) and (orientation: landscape) {
+        font-size: 1.2rem;
+        margin-top: -20px;
+    }
 
-@media (max-width: ${({ theme }) => theme.mobileXS}) {
-    font-size: 1.15rem;
-}
+    @media (max-width: ${({ theme }) => theme.mobileXS}) {
+        font-size: 1.15rem;
+    }
 
-@media (max-width: ${({ theme }) => theme.mobileXS}) and (orientation: landscape) {
-    font-size: 1rem;
-    margin-top: -20px;
-}
+    @media (max-width: ${({ theme }) => theme.mobileXS}) and (orientation: landscape) {
+        font-size: 1rem;
+        margin-top: -20px;
+    }
 
-@media (max-width: ${({ theme }) => theme.mobileXXS}) {
-    font-size: 1rem;
-}
-`
-
-export const SocialContainer = styled.div`
-
-`
+    @media (max-width: ${({ theme }) => theme.mobileXXS}) {
+        font-size: 1rem;
+    }
+`;
 
 export const SocialWrapper = styled.div`
-    
-`
+    min-width: 15%;
+    width: 20%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+`;
 
 export const SocialLink = styled.a`
     color: #fff;
     font-size: 1.5rem;
     cursor: pointer;
     transition: color 0.3s linear;
-    position: relative;
-    top: 30px;
 
     &:hover {
-        color: #66FCF1;
+        color: #66fcf1;
     }
-
-@media (max-width: ${({ theme }) => theme.mobileL}) and (orientation: landscape) {
-    top: 10px;
-}
-
-@media (max-width: ${({ theme }) => theme.mobileM}) and (orientation: landscape) {
-    top: 10px;
-}
-`
+`;
